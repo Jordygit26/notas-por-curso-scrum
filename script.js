2f52f6ef6cd446e1aaaab826dbbe1df40dba61b8
Paolo Lopez <okihnnj123@gmail.com>
2026-01-21 15:39:58 -0500

    validaciones y mensajes del formulario

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-// variables globales
-let notas = [];
+// =====================
+// las validaciones y mesnajes
+// =====================
 
+function validarCurso(curso) {
+    return typeof curso === 'string' && curso.trim().length >= 2;
+}
+
+function validarNota(nota) {
+    return !isNaN(nota) && nota >= 0 && nota <= 20;
+}
+
+function mostrarMensaje(texto, esError = false) {
+    const msgSuccess = document.getElementById('msg-success');
+    const msgError = document.getElementById('msg-error');
+
+    msgSuccess.classList.add('oculto');
+    msgError.classList.add('oculto');
+
+    if (esError) {
+        msgError.textContent = texto;
+        msgError.classList.remove('oculto');
+    } else {
+        msgSuccess.textContent = texto;
+        msgSuccess.classList.remove('oculto');
+    }
+
+    setTimeout(() => {
+        msgSuccess.classList.add('oculto');
+        msgError.classList.add('oculto');
+    }, 3000);
+}
